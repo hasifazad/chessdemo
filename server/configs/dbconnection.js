6b6c7b56d559b3da307d6c8bfbce67674f360e43
hasif_azad <asifazad114@gmail.com>
2023-03-23 15:26:47 +0530

    update

--- server/configs/dbconnection.js
+++ server/configs/dbconnection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
-
+const MONGO_USER = process.env.MONGO_USER,
+    MONGO_PASSWORD = process.env.MONGO_PASSWORD,
+    MONGO_URL = process.env.MONGO_URL
 const db = () => {
-    mongoose.connect('mongodb://localhost:27017/chess').then((res) => {
+    mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_URL}`).then((res) => {
         console.log('database connected');
     }).catch((err) => {
         console.log(err);
